d7d071ce384f53a9eb8931c630a654b58792d585
derFunk <github@photono.de>
2012-08-11 16:41:52 +0200

    code formatting…

--- unity3d-jslogger-project/Assets/Scripts/JsLoggerCallback.cs
+++ unity3d-jslogger-project/Assets/Scripts/JsLoggerCallback.cs
@@ -5,35 +5,35 @@ using ChimeraEntertainment.Unity3DJavascriptLogger;
 public class JsLoggerCallback : MonoBehaviour
 {
 	#if UNITY_WEBPLAYER && !UNITY_EDITOR
-
+	
 	JavascriptLogger jsLogger;
 		
 	void Awake()
-    {
+	{
 		jsLogger = new JavascriptLogger();
 		
 		// Register the javascript logger for the standard unity log output.
-        Application.RegisterLogCallback(jsLogger.HandleUnityLog);
+	    Application.RegisterLogCallback(jsLogger.HandleUnityLog);
 		
 		Debug.Log("Log-Test: Javascript Logging Handler enabled!");
 		Debug.LogWarning("LogWarning-Test: Javascript Logging Handler enabled!");
 		Debug.LogError("LogError-Test: Javascript Logging Handler enabled!");
-    }
+	}
 	
 	void Update()
 	{
 		jsLogger.Dispatcher.Dequeue();
 	}
-    
+	
 	void OnDisable()
-    {
-        Application.RegisterLogCallback(null);
-    }
-
-    void OnDestroy()
-    {
-        Application.RegisterLogCallback(null);
-    }
+	{
+	    Application.RegisterLogCallback(null);
+	}
+	
+	void OnDestroy()
+	{
+	    Application.RegisterLogCallback(null);
+	}
 
 #endif
 }
